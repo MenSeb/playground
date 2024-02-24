1a1fc652f772254e42b2758fb07d31cfcb144e8a
biodiscus <biodiscus@gmail.com>
2024-02-24 18:51:35 +0100

    refactor: optimize webpack config for usage HtmlBundlerPlugin with handlebars

--- src/client/scripts/index.js
+++ src/client/scripts/index.js
@@ -1,11 +1,11 @@
-// import { Carousel } from './carousel.js';
-// import { Navigation } from './navigation.js';
-// import { Menu } from './menu.js';
+import { Carousel } from './carousel.js';
+import { Navigation } from './navigation.js';
+import { Menu } from './menu.js';
 
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
-        // new Menu();
-        // new Navigation();
-        // new Carousel({ id: 'carousel-banner' });
+        new Menu();
+        new Navigation();
+        new Carousel({ id: 'carousel-banner' });
     });
 })();
